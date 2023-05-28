9c959d6da6b847f5393691ac2e71497daf91084d
kevin <kevin.saffioti@gmail.com>
2023-05-28 15:35:38 +0200

    remove firestore implementation

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,9 +3,9 @@ import { getAuth } from 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useRouter } from 'next/navigation';
 import { initFirebase } from '../firebase/firebaseApp';
+import Dashboard from '../components/Dashboard';
 
 import Loading from '../components/Loading';
-
 
 export default function Home() {
 	const app = initFirebase();
@@ -13,16 +13,12 @@ export default function Home() {
 	const [user, loading] = useAuthState(auth);
 	const router = useRouter();
 
-
-    if (loading){
-        return (
-          <Loading/>)
-      }
-      if (!user){
-        router.push('/login')
-        return<div className="flex justify-center items-center h-screen bg-blue-500 text-white">Please Login to continue</div>
-      }
-  return (
-    <Dashboard/>
-  )
+	if (loading) {
+		return <Loading />;
+	}
+	if (!user) {
+		router.push('/login');
+		return <div className='flex justify-center items-center h-screen bg-blue-500 text-white'>Please Login to continue</div>;
+	}
+	return <Dashboard />;
 }
